6048833b7a8f8089c2b686790a782313571e81b3
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-06-08 23:23:13 +0530

    changes

--- section6/v1-springboot/accounts/src/main/java/com/microservice/accounts/Controller/AccountController.java
+++ section6/v1-springboot/accounts/src/main/java/com/microservice/accounts/Controller/AccountController.java
@@ -14,21 +14,33 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(
         name = "Account Controller REST API ", description = "Account Controller CRUD REST API Microservices")
+
 @RestController
 @RequestMapping(path = "/api",produces = MediaType.APPLICATION_JSON_VALUE)
-@AllArgsConstructor
+
 @Validated
 public class AccountController {
 
-    private AccuntService accuntService;
+    private final AccuntService accuntService;
+
+    @Value("${build.version}")
+    private String buildVersion;
+
+    public AccountController(AccuntService accuntService) {
+        this.accuntService = accuntService;
+    }
+
     @Operation(
             summary = "Create Account REST API",
             description = "REST API to create new Customer &  Account inside Bank"
@@ -48,11 +60,11 @@ public class AccountController {
     }
     )
     @PostMapping("/create")
-    public ResponseEntity<ResponseDto>createAccount( @Valid @RequestBody CustomerDto customerDto){
-      accuntService.createAccount(customerDto);
-     return ResponseEntity
-             .status(HttpStatus.CREATED)
-             .body(new ResponseDto(AccountConstants.STATUS_201,AccountConstants.MESSAGE_201));
+    public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
+        accuntService.createAccount(customerDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountConstants.STATUS_201, AccountConstants.MESSAGE_201));
     }
 
     @Operation(
@@ -75,11 +87,11 @@ public class AccountController {
     )
 
     @GetMapping("/fetch")
-    public ResponseEntity<CustomerDto>fetchAccount(@RequestParam
-            @Pattern(regexp = "[0-9]{10}",message = "Mobile number should be 10 digits")
-                                                       String mobileNumber){
+    public ResponseEntity<CustomerDto> fetchAccount(@RequestParam
+                                                    @Pattern(regexp = "[0-9]{10}", message = "Mobile number should be 10 digits")
+                                                    String mobileNumber) {
         CustomerDto customerDto = accuntService.fetchAccount(mobileNumber);
-         return ResponseEntity.status(HttpStatus.OK).body(customerDto); 
+        return ResponseEntity.status(HttpStatus.OK).body(customerDto);
     }
 
     @Operation(
@@ -106,12 +118,12 @@ public class AccountController {
     )
 
     @PutMapping("/update")
-    public ResponseEntity<ResponseDto>updateAccount(@Valid @RequestBody CustomerDto customerDto){
+    public ResponseEntity<ResponseDto> updateAccount(@Valid @RequestBody CustomerDto customerDto) {
         boolean isUpdated = accuntService.updateAccount(customerDto);
-        if(isUpdated){
-            return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(AccountConstants.STATUS_200,AccountConstants.MESSAGE_200));
-        }else{
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseDto(AccountConstants.STATUS_500,AccountConstants.MESSAGE_500));
+        if (isUpdated) {
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(AccountConstants.STATUS_200, AccountConstants.MESSAGE_200));
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseDto(AccountConstants.STATUS_500, AccountConstants.MESSAGE_500));
         }
     }
 
@@ -135,14 +147,19 @@ public class AccountController {
     }
     )
     @DeleteMapping("/delete")
-    public ResponseEntity<ResponseDto>deleteAccount(@RequestParam
-            @Pattern(regexp = "[0-9]{10}",message = "Mobile number should be 10 digits")
-                                                        String mobileNumber){
+    public ResponseEntity<ResponseDto> deleteAccount(@RequestParam
+                                                     @Pattern(regexp = "[0-9]{10}", message = "Mobile number should be 10 digits")
+                                                     String mobileNumber) {
         boolean isDeleted = accuntService.deleteAccount(mobileNumber);
-        if(isDeleted){
-            return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(AccountConstants.STATUS_200,AccountConstants.MESSAGE_200));
-        }else{
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseDto(AccountConstants.STATUS_500,AccountConstants.MESSAGE_500));
+        if (isDeleted) {
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(AccountConstants.STATUS_200, AccountConstants.MESSAGE_200));
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseDto(AccountConstants.STATUS_500, AccountConstants.MESSAGE_500));
         }
+    }
+
+    @GetMapping("/version")
+    public ResponseEntity<String> getBuildVersion() {
+        return ResponseEntity.status(HttpStatus.OK).body(buildVersion);
     }
 }
