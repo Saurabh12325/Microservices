897c012626df8af8df4c6073c2f13229129c24ec
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-06-09 00:05:03 +0530

    changes

--- section6/v1-springboot/accounts/src/main/java/com/microservice/accounts/dto/AccountContactInfoDto.java
+++ section6/v1-springboot/accounts/src/main/java/com/microservice/accounts/dto/AccountContactInfoDto.java
@@ -1,4 +1,12 @@
 package com.microservice.accounts.dto;
 
-public record AccountContactInfoDto() {
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+import java.util.Map;
+
+@ConfigurationProperties(prefix = "accounts")
+public record AccountContactInfoDto(String message, Map<String,String> contactDetails , List<String> onCallSupport) {
+
+
 }
