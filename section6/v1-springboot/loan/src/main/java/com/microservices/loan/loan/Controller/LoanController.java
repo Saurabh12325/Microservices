c6d6af2b3f528bce91fea0f4f5abd74b862663f4
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-06-10 06:40:35 +0530

    spring configuration profiles

--- section6/v1-springboot/loan/src/main/java/com/microservices/loan/loan/Controller/LoanController.java
+++ section6/v1-springboot/loan/src/main/java/com/microservices/loan/loan/Controller/LoanController.java
@@ -2,22 +2,35 @@ package com.microservices.loan.loan.Controller;
 
 import com.microservices.loan.loan.Constants.LoanConstants;
 import com.microservices.loan.loan.Service.LoanService;
+import com.microservices.loan.loan.dto.LoanContactInfoDto;
 import com.microservices.loan.loan.dto.LoanDto;
 import com.microservices.loan.loan.dto.ResponseDto;
 import com.microservices.loan.loan.entity.Loan;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.web.ReactiveOffsetScrollPositionHandlerMethodArgumentResolver;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@AllArgsConstructor
+
 @RestController
 @RequestMapping("/loan")
 public class LoanController {
-    private LoanService loanService;
+    private final LoanService loanService;
+
+    public LoanController(LoanService loanService) {
+        this.loanService = loanService;
+    }
+
+    @Value("${build.version}")
+    private String buildVersion;
+
+    @Autowired
+    private LoanContactInfoDto loanContactInfoDto;
 
     @PostMapping("/createLoan")
     public ResponseEntity<ResponseDto> createLoan(@RequestParam String mobileNumber) {
@@ -57,5 +70,14 @@ public class LoanController {
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(new ResponseDto(LoanConstants.STATUS_417,LoanConstants.MESSAGE_417_DELETE));
         }
+    }
+
+    @GetMapping("/version")
+    public ResponseEntity<String> getBuildVersion(){
+        return ResponseEntity.status(HttpStatus.OK).body(buildVersion);
+    }
+    @GetMapping("/loanInfo")
+    public ResponseEntity<LoanContactInfoDto> getLoanContactInfo() {
+        return ResponseEntity.status(HttpStatus.OK).body(loanContactInfoDto);
     }
 }
