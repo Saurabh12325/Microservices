efb31b3d5320622c92a96bd2a8573b457b28dfed
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-06-10 05:57:40 +0530

    spring profiles

---
+++ section6/v1-springboot/cards/src/main/java/com/microservice/card/DTO/CardContactInfoDto.java
@@ -0,0 +1,9 @@
+package com.microservice.card.DTO;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+import java.util.Map;
+@ConfigurationProperties(prefix = "cards")
+public record CardContactInfoDto (String message, Map<String,String> contactDetails, List<String> onCallSupport) {
+}
