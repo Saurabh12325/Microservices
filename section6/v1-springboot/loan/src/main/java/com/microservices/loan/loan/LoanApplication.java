ca7d830f86ea086877ae94007e7ada9d79d67496
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-06-10 06:35:49 +0530

    spring configuration profiles

--- section6/v1-springboot/loan/src/main/java/com/microservices/loan/loan/LoanApplication.java
+++ section6/v1-springboot/loan/src/main/java/com/microservices/loan/loan/LoanApplication.java
@@ -1,10 +1,13 @@
 package com.microservices.loan.loan;
 
+import com.microservices.loan.loan.dto.LoanContactInfoDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing(auditorAwareRef = "AuditorAwareImpl")
+@EnableConfigurationProperties(value = LoanContactInfoDto.class)
 @SpringBootApplication
 public class LoanApplication {
 
